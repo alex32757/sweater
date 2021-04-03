906ac622c0511e53908ae35a654cbf703e5c03c2
Alexandr Panarin <alex32757@gmail.com>
2021-04-03 17:19:30 +0300

    Add recapthca

--- src/main/java/org/example/sweater/controller/RegisrtationController.java
+++ src/main/java/org/example/sweater/controller/RegisrtationController.java
@@ -1,34 +1,66 @@
 package org.example.sweater.controller;
 
 import org.example.sweater.domain.User;
+import org.example.sweater.domain.dto.CapthcaResponseDto;
 import org.example.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
+import java.util.Collections;
 import java.util.Map;
 
 @Controller
 public class RegisrtationController {
+    private final static String CAPTHCA_URL = "https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s";
+
     @Autowired
     private UserService userService;
+
+    @Value("${recapthca.secret}")
+    private String secret;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/registration")
     public String registration() {
         return "registration";
     }
 
     @PostMapping("/registration")
-    public String addUser(@Valid User user, BindingResult bindingResult, Model model) {
-        if (user.getPassword() != null && !user.getPassword().equals(user.getPassword2())) {
+    public String addUser(
+            @RequestParam("password2") String passwordConfirm,
+            @RequestParam("g-recaptcha-response") String capthcaResponse,
+            @Valid User user,
+            BindingResult bindingResult, Model model
+    ) {
+        String url = String.format(CAPTHCA_URL, secret, capthcaResponse);
+        CapthcaResponseDto response = restTemplate.postForObject(url, Collections.emptyList(), CapthcaResponseDto.class);
+
+        if (!response.isSuccess()) {
+            model.addAttribute("captchaError", "Fill captcha");
+        }
+
+        boolean isConfirmEmpty = StringUtils.isEmpty(passwordConfirm);
+        if (isConfirmEmpty) {
+            model.addAttribute("password2Error", "Password confirmation can't be empty");
+        }
+
+        if (user.getPassword() != null && !user.getPassword().equals(passwordConfirm)) {
             bindingResult.addError(new FieldError("user", "password", "Passwords are different!"));
         }
-        if (bindingResult.hasErrors()) {
+        if (isConfirmEmpty || bindingResult.hasErrors() || !response.isSuccess()) {
             Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
 
             model.mergeAttributes(errors);
@@ -47,8 +79,10 @@ public class RegisrtationController {
         boolean isActivated = userService.activateUser(code);
 
         if (isActivated) {
+            model.addAttribute("messageType", "success");
             model.addAttribute("message", "User successfully activated");
         } else {
+            model.addAttribute("messageType", "danger");
             model.addAttribute("message", "Activation code is not found");
         }
 
--- src/main/java/org/example/sweater/domain/User.java
+++ src/main/java/org/example/sweater/domain/User.java
@@ -19,8 +19,6 @@ public class User implements UserDetails {
     private String username;
     @NotBlank(message = "Password can't be empty")
     private String password;
-    @Transient
-    private String password2;
     private boolean active;
 
     @Email(message = "Email isn't correct")
@@ -118,11 +116,4 @@ public class User implements UserDetails {
         this.activationCode = activationCode;
     }
 
-    public String getPassword2() {
-        return password2;
-    }
-
-    public void setPassword2(String password2) {
-        this.password2 = password2;
-    }
 }
